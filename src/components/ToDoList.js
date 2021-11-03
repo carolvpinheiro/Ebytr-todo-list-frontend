1ad65ac4d999f2c28b8e738fae2cb217cead8340
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-11-03 20:33:30 -0300

    Feat: to-do list with task inputs

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class ToDoList extends Component {
   render() {
+    const { listTasks } = this.props;
+
     return (
       <div>
-        
+        <ul>
+          { listTasks.map((task) => (<li key={task}>{task}</li>)) }
+        </ul>
       </div>
     );
   }
 }
+
+ToDoList.propTypes = {
+  listTasks: PropTypes.arrayOf.isRequired,
+};
 
 export default ToDoList;
